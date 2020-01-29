4c32aa27f8e5cb855f384dde54a8e299864b6204
master <master@rivne.itstep.org>
2020-01-29 11:28:12 +0200

    Added Phone view and static files

--- IntroShop/Main/MockData/MockPhones.cs
+++ IntroShop/Main/MockData/MockPhones.cs
@@ -20,7 +20,7 @@ namespace IntroShop.Main.MockData
                     {
                         name = "Samsung S10",
                         description = "Best phone by Samsung",
-                        img = "https://i1.rozetka.ua/goods/11052575/samsung_galaxy_s10_6_128_gb_white_sm_g973fzwdsek_images_11052575871.jpg",
+                        img = "/img/samsung_galaxy_s10_6_128_gb.jpg",
                         price = 23000,
                         Category = _categoryPhone.AllCategories.First()
                     },
@@ -28,7 +28,7 @@ namespace IntroShop.Main.MockData
                     {
                         name = "Nokia 3310",
                         description = "Old phone.",
-                        img = "https://static.turbosquid.com/Preview/001329/666/6L/_DHQ.jpg",
+                        img = "/img/nokia_3310.jpg",
                         price = 700,
                         Category = _categoryPhone.AllCategories.Last()
                     }
